658e16b58f0888609f2a17f37820c20c4cae8355
Alain Léglise <alainleglise@outlook.fr>
2023-04-13 16:57:20 +0200

    Suppression import inutile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { K_About } from '../menuItems';
 import About from '../routes/About';
 import Home from '../routes/Home';
